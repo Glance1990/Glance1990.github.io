ac3f0cb6195b566e34ab7ae41de855ac9085563a
Pavlo Kadysh <paulkadysh@gmail.com>
2016-09-22 13:46:16 +0300

    22092016_Validator

--- Landing_Gold-Coach/js/common.js
+++ Landing_Gold-Coach/js/common.js
@@ -1,49 +1,32 @@
-$(document).on('ready', function() {
-     
-    //   $(".regular").slick({
-    //     infinite: true,
-    //     slidesToShow: 5,
-    //     slidesToScroll: 1
-    //   });
-    //   $(".center").slick({
-    //     dots: true,
-    //     infinite: true,
-    //     centerMode: true,
-    //     slidesToShow: 3,
-    //     slidesToScroll: 3
-    //   });
-    //   $(".variable").slick({
-    //     dots: true,
-    //     infinite: true,
-    //     variableWidth: true
-    //   });
-    // });
-
-    $('.center').slick({
-  centerMode: true,
-  centerPadding: '60px',
-  slidesToShow: 5,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
-});
-
-
+$('.responsive').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        }, {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
 });
